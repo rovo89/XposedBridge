0b05df647266092588ed5c2f895f25f29c7963b2
Stefan Siegel <ssiegel@sdas.net>
2015-03-26 05:51:01 +0100

    Add extra files to XposedBridge.jar

--- Android.mk
+++ Android.mk
@@ -9,5 +9,6 @@ LOCAL_SRC_FILES := \
         $(call all-java-files-under, lib/apache-commons-lang/external)
 
 include $(BUILD_JAVA_LIBRARY)
+$(LOCAL_INTERMEDIATE_TARGETS): PRIVATE_EXTRA_JAR_ARGS := -C "$(LOCAL_PATH)" assets -C "$(LOCAL_PATH)" NOTICE.txt
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
